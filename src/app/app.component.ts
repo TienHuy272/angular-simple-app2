af5276ba0a1af5b392f3d2f1cd6da81ba1dba5a0
HuyNT <tienhuylionel@gmail.com>
2024-10-29 18:28:59 +0700

    using ng module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,7 @@
-import { Component, signal } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { UserInputComponent } from './user-input/user-input.component';
-import { InvestmentResultsComponent } from './investment-results/investment-results.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent]
 })
 export class AppComponent {}
